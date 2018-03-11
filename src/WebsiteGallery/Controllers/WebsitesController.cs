daab889198b09340069928e54581d31b9aad8aea
Jake Ford <jake@jakeford.io>
2018-03-10 18:16:39 -0600

    Added Detail Action Method

--- src/WebsiteGallery/Controllers/WebsitesController.cs
+++ src/WebsiteGallery/Controllers/WebsitesController.cs
@@ -6,7 +6,11 @@ using System.Web.Mvc;
 
 namespace WebsiteGallery.Controllers
 {
-    public class WebsitesController : Controller
+    public class WebsitesController : Controller //Controller Classes must be public to access in URL
     {
+        public string Detail() //Controller ACTION METHODS must also be public to access in URL
+        {
+            return "What up my nigga.";
+        }
     }
 }
