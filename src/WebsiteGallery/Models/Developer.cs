68add0217272e43952e2f22ba7535e7707285cba
Jake Ford <jake@jakeford.io>
2018-03-13 21:39:14 -0500

    Added Website.cs and Developer.cs data models to Models folder

---
+++ src/WebsiteGallery/Models/Developer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebsiteGallery.Models {
+    public class Developer {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string[] Languages { get; set; }
+    }
+}
---
+++ src/WebsiteGallery/Models/Website.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebsiteGallery.Models {
+    public class Website {
+        public int Id { get; set; }
+        public string ProjectTitle { get; set; }
+        public Developer[] Developers { get; set; }
+        public string Link { get; set; }
+        public string[] Requirements { get; set; }
+        public string[] Technologies { get; set; }
+    }
+}
